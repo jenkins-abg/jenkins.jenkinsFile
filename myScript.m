5e6adb45819c214edb43fd56296a3d23511b7af0
jenkins-abg <69100616+jenkins-abg@users.noreply.github.com>
2022-06-28 10:03:35 +0800

    Update myScript.m

--- myScript.m
+++ myScript.m
@@ -9,3 +9,19 @@ hGui = findobj('Tag','tformChoiceGui');
 waitfor(hGui);
 
 fprintf('continuing script..\n');
+
+
+@echo off
+set "ZIP=C:\Program Files\7-Zip\7z.exe"
+set "ARGS=%*"
+setlocal EnableDelayedExpansion
+for %%F in (!ARGS!) do (
+    endlocal
+    dir /A "%%~fF" | > nul 2>&1 findstr "DIR" && (
+        "%ZIP%" a -tzip "%%~F.zip" "%%~F\*" -mx0
+    ) || (
+        "%ZIP%" a -tzip "%%~F.zip" "%%~fF" -mx0
+    )
+    setlocal
+)
+endlocal
