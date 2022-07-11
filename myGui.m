2a10e9d47ba38a2d527fb3098adcae1d53acea05
jenkins-abg <69100616+jenkins-abg@users.noreply.github.com>
2022-07-11 16:36:14 +0800

    Update myGui.m

--- myGui.m
+++ myGui.m
@@ -38,3 +38,16 @@ H = ancestor(ButtonH, 'figure');
 set(H, 'UserData', Num);
 uiresume(H);
 end
+
+
+echo on
+
+for /f "tokens=3,2,4 delims=/- " %%x in ("%date%") do set d=%%y%%x%%z
+
+set data=%d%
+
+Echo zipping...
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip "C:\twc.zip" "C:\MyDocuments\*.doc"
+
+echo Done!
